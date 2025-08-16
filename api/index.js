1ff57066c468a0596af38430b3eccfa8812a8801
Saifalikhan99 <myname14saif@gmail.com>
2025-08-16 12:05:15 +0530

    fix: reorder middleware setup in index.js for proper configuration

--- api/index.js
+++ api/index.js
@@ -6,14 +6,20 @@ import router from "../src/route.js";
 // const port = 3001;
 const app = express();
 
-app.use(cors());
-app.use(fileupload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(fileupload());
 app.get("/", (req, res) => {
   res.status(200).send("Server is running");
 });
 // app.use(router);
-app.listen(3001)
+app.listen(3001);
 
 export default app;
