e4be1cdbbff5ded0789b3754e1e2daabc42f5151
Saifalikhan99 <myname14saif@gmail.com>
2025-08-16 12:08:59 +0530

    fix: comment out middleware setup in index.js to prevent conflicts

--- api/index.js
+++ api/index.js
@@ -6,15 +6,15 @@ import router from "../src/route.js";
 // const port = 3001;
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET", "POST"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET", "POST"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 // app.use(fileupload());
 app.get("/", (req, res) => {
   res.status(200).send("Server is running");
