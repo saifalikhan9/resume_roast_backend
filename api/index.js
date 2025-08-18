3ea50e8fe2824c5e8e4a24945b6f83ff70b9dc2b
Saifalikhan99 <myname14saif@gmail.com>
2025-08-18 12:57:10 +0530

    fix: configure CORS and add rate limiting to enhance API security

--- api/index.js
+++ api/index.js
@@ -2,13 +2,25 @@ import express from "express";
 import cors from "cors";
 import roastService from "../src/roastService.js";
 import multer from "multer";
+import { configDotenv } from "dotenv";
 
 const app = express();
+configDotenv();
 
-app.use(cors());
+app.use(cors({
+  origin : process.env.FRONTEND_URL,
+  methods: ['GET','POST']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+const apiLimiter = rateLimit({
+    windowMs: 2 * 60 * 1000, // 2 minutes
+    max: 10,
+    message: "Too many requests from this IP, please try again after 15 minutes",
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use(apiLimiter)
 app.get("/", (req, res) => {
   res.status(200).send("Server is running 🚀");
 });
@@ -17,16 +29,14 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 app.post("/generateRoast", upload.single("resume"), roastService);
 
-// app.use(router);
-// app.use((err, req, res, next) => {
-//   console.error("❌ Global error:", err);
-//   res.status(500).json({ message: "Internal Server Error" });
-// });
 
-// ✅ Export for Vercel
+app.use((err, req, res, next) => {
+  console.error("❌ Global error:", err);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
 export default app;
 
-// ✅ Local dev support
 if (process.env.NODE_ENV !== "production") {
   app.listen(3001, () => {
     console.log("Server running on http://localhost:3001");
